e9211e778d8ff67ffb69aea454de3703b0c0c133
ZOI-dayo <zoizoi.dayo@gmail.com>
2025-03-07 23:05:39 +0900

    'num_iterations': 500,

--- main.py
+++ main.py
@@ -367,7 +367,7 @@ def train_fold(train_X: pd.DataFrame, train_y: pd.Series, valid_X: pd.DataFrame,
         'bagging_fraction': 0.8,
         'num_leaves': 31,
         'random_state': 0,
-        'num_iterations': 1000,
+        'num_iterations': 500,
     }
 
     # 学習. auc が 100ステップ以上改善しないなら打ち切るように設定する
